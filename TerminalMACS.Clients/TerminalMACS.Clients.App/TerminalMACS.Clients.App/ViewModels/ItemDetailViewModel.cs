dc30d0934cc3a0186a14aa8fa4e8947d5a0b7be4
lsq6.com <632871194@qq.com>
2020-03-21 01:58:25 +0800

    添加移动端代码

---
+++ TerminalMACS.Clients/TerminalMACS.Clients.App/TerminalMACS.Clients.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TerminalMACS.Clients.App.Models;
+
+namespace TerminalMACS.Clients.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
