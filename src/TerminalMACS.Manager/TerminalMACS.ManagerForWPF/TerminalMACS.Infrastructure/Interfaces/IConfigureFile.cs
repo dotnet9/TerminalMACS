7699fa5040376055d6d01511188a75ee784e4bc8
沙漠尽头的狼(dotnet9.com) <632871194@qq.com>
2020-04-14 17:35:37 +0800

    add wpf manage

---
+++ src/TerminalMACS.Manager/TerminalMACS.ManagerForWPF/TerminalMACS.Infrastructure/Interfaces/IConfigureFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TerminalMACS.Infrastructure
+{
+    public class ValueChangedEventArgs : EventArgs
+    {
+        public string KeyName { get; }
+
+        public ValueChangedEventArgs(string keyName) => KeyName = keyName;
+    }
+
+    public interface IConfigureFile
+    {
+        event EventHandler<ValueChangedEventArgs> ValueChanged;
+
+        bool Contains(string key);
+
+        T GetValue<T>(string key);
+
+        IConfigureFile SetValue<T>(string key, T value);
+
+        IConfigureFile Load(string filePath = null);
+
+        IConfigureFile Clear();
+
+        void Delete();
+    }
+}
---
+++ src/TerminalMACS.Manager/TerminalMACS.ManagerForWPF/TerminalMACS/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TerminalMACS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
